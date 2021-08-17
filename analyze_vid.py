0d2886f8f16de160199dfd0ba46a4800ee69179a
emmanuel <emmanuel.montblanc@insa-lyon.fr>
2021-08-17 09:22:26 +0200

    buttons are now in a frame

--- analyze_vid.py
+++ analyze_vid.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-
+import sys
 from PyQt5.QtCore import Qt, QPoint, QTimer, QRect
 from PyQt5.QtGui import QPixmap, QPainter, QPen, QKeySequence
-from PyQt5.QtWidgets import QMainWindow, QShortcut, QPushButton, QLabel, QTextEdit, QVBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QShortcut, QPushButton, QLabel, QTextEdit, QFrame, QApplication
 
 from recorder import Recorder
 
@@ -59,90 +59,96 @@ class AnalyzeVidWindow(QMainWindow):
 
         self.help_wdw = QMainWindow()
 
+        self.frame = QFrame(self)
+        self.frame.setLineWidth(1)
+        self.frame.setGeometry(self.pixmap.width(), 20,
+                               self.frameGeometry().width(), self.frameGeometry().height())
+
+        # Help button
+        self.button_help = QPushButton(self.frame)
+        self.button_help.setText("help")
+        self.button_help.setGeometry(10, 0, 80, 40)
+        self.button_help.clicked.connect(self.help)
+
+        # change video button
+        self.button_change_vid = QPushButton(self.frame)
+        self.button_change_vid.setText("change video")
+        self.button_change_vid.setGeometry(10, 50, 80, 40)
+        self.button_change_vid.clicked.connect(self.change_video)
+
+        # Record button
+        self.button_record = QPushButton(self.frame)
+        self.button_record.setText("start recording")
+        self.button_record.setGeometry(10, 100, 80, 40)
+        self.button_record.clicked.connect(self.record_video)
+
+        # Zoom button
+        self.button_zoom = QPushButton(self.frame)
+        self.button_zoom.setText("zoom")
+        self.button_zoom.setGeometry(10, 150, 80, 40)
+        self.button_zoom.clicked.connect(self.zoom_image)
+
+        # Label for choosing colors
+        self.color_label = QLabel(self.frame)
+        self.color_label.setText("drawing color:")
+        self.color_label.setGeometry(10, 200, 80, 20)
+
+        # green color button
+        self.button_green = QPushButton(self.frame)
+        self.button_green.setStyleSheet("background-color: green")
+        self.button_green.setGeometry(5, 220, 40, 40)
+        self.button_green.clicked.connect(lambda: self.select_color(Qt.darkGreen))
+
+        # red color button
+        self.button_red = QPushButton(self.frame)
+        self.button_red.setStyleSheet("background-color: red")
+        self.button_red.setGeometry(50, 220, 40, 40)
+        self.button_red.clicked.connect(lambda: self.select_color(Qt.red))
+
         # Undo button
-        self.button_ctrlz = QPushButton(self)
+        self.button_ctrlz = QPushButton(self.frame)
         self.button_ctrlz.setText("undo")
-        self.button_ctrlz.setGeometry(self.frameGeometry().width() - 85, 50, 80, 40)
+        self.button_ctrlz.setGeometry(10, 270, 80, 40)
         self.button_ctrlz.clicked.connect(self.remove_last_line)
+
+        # Label for choosing speed
+        self.speed_label = QLabel(self.frame)
+        self.speed_label.setText("playing speed:")
+        self.speed_label.setGeometry(10, 320, 80, 20)
+
+        # x1 speed button
+        self.speed_x1 = QPushButton(self.frame)
+        self.speed_x1.setText("x1")
+        self.speed_x1.setGeometry(10, 340, 80, 20)
+        self.speed_x1.clicked.connect(lambda: self.set_speed(1))
+
+        # x0.5 speed button
+        self.speed_x05 = QPushButton(self.frame)
+        self.speed_x05.setText("x0.5")
+        self.speed_x05.setGeometry(10, 360, 80, 20)
+        self.speed_x05.clicked.connect(lambda: self.set_speed(0.5))
+
+        # x0.25 speed button
+        self.speed_x025 = QPushButton(self.frame)
+        self.speed_x025.setText("x0.25")
+        self.speed_x025.setGeometry(10, 380, 80, 20)
+        self.speed_x025.clicked.connect(lambda: self.set_speed(0.25))
+
+        # x0.125 speed button
+        self.speed_x0125 = QPushButton(self.frame)
+        self.speed_x0125.setText("x0.125")
+        self.speed_x0125.setGeometry(10, 400, 80, 20)
+        self.speed_x0125.clicked.connect(lambda: self.set_speed(0.125))
+
+        # Play button
+        self.button_play_pause = QPushButton(self.frame)
+        self.button_play_pause.setText("Play")
+        self.button_play_pause.setGeometry(10, 430, 80, 40)
+        self.button_play_pause.clicked.connect(self.play_pause_vid)
 
         # Undo shortcut
         self.ctrlz = QShortcut(QKeySequence('Ctrl+Z'), self)
         self.ctrlz.activated.connect(self.remove_last_line)
-
-        # Label for choosing colors
-        self.color_label = QLabel(self)
-        self.color_label.setText("drawing color:")
-        self.color_label.setGeometry(self.frameGeometry().width() - 80, 150, 80, 40)
-
-        # green color button
-        self.button_green = QPushButton(self)
-        self.button_green.setStyleSheet("background-color: green")
-        self.button_green.setGeometry(self.frameGeometry().width() - 85, 180, 40, 40)
-        self.button_green.clicked.connect(lambda: self.select_color(Qt.darkGreen))
-
-        # red color button
-        self.button_red = QPushButton(self)
-        self.button_red.setStyleSheet("background-color: red")
-        self.button_red.setGeometry(self.frameGeometry().width() - 42, 180, 40, 40)
-        self.button_red.clicked.connect(lambda: self.select_color(Qt.red))
-
-        # Label for choosing colors
-        self.speed_label = QLabel(self)
-        self.speed_label.setText("playing speed:")
-        self.speed_label.setGeometry(self.frameGeometry().width() - 80, 250, 80, 40)
-
-        # x1 speed button
-        self.speed_x1 = QPushButton(self)
-        self.speed_x1.setText("x1")
-        self.speed_x1.setGeometry(self.frameGeometry().width() - 85, 280, 80, 20)
-        self.speed_x1.clicked.connect(lambda: self.set_speed(1))
-
-        # x0.5 speed button
-        self.speed_x05 = QPushButton(self)
-        self.speed_x05.setText("x0.5")
-        self.speed_x05.setGeometry(self.frameGeometry().width() - 85, 300, 80, 20)
-        self.speed_x05.clicked.connect(lambda: self.set_speed(0.5))
-
-        # x0.25 speed button
-        self.speed_x025 = QPushButton(self)
-        self.speed_x025.setText("x0.25")
-        self.speed_x025.setGeometry(self.frameGeometry().width() - 85, 320, 80, 20)
-        self.speed_x025.clicked.connect(lambda: self.set_speed(0.25))
-
-        # x0.125 speed button
-        self.speed_x0125 = QPushButton(self)
-        self.speed_x0125.setText("x0.125")
-        self.speed_x0125.setGeometry(self.frameGeometry().width() - 85, 340, 80, 20)
-        self.speed_x0125.clicked.connect(lambda: self.set_speed(0.125))
-
-        # Play button
-        self.button_play_pause = QPushButton(self)
-        self.button_play_pause.setText("Play")
-        self.button_play_pause.setGeometry(self.frameGeometry().width() - 85, 380, 80, 40)
-        self.button_play_pause.clicked.connect(self.play_pause_vid)
-
-        # Record button
-        self.button_record = QPushButton(self)
-        self.button_record.setText("start recording")
-        self.button_record.setGeometry(self.frameGeometry().width() - 85, 450, 80, 40)
-        self.button_record.clicked.connect(self.record_video)
-
-        # change video button
-        self.button_change_vid = QPushButton(self)
-        self.button_change_vid.setText("change video")
-        self.button_change_vid.setGeometry(self.frameGeometry().width() - 85, 500, 80, 40)
-        self.button_change_vid.clicked.connect(self.change_video)
-
-        # Zoom button
-        self.button_zoom = QPushButton(self)
-        self.button_zoom.setText("zoom")
-        self.button_zoom.setGeometry(self.frameGeometry().width() - 85, 550, 80, 40)
-        self.button_zoom.clicked.connect(self.zoom_image)
-
-        self.button_help = QPushButton(self)
-        self.button_help.setText("help")
-        self.button_help.setGeometry(self.frameGeometry().width() - 85, 600, 80, 40)
-        self.button_help.clicked.connect(self.help)
 
         # Play pause shortcut
         self.space_bar = QShortcut(QKeySequence(Qt.Key_Space), self)
@@ -364,3 +370,7 @@ class HelpWindow(QMainWindow):
 def my_round(x, base=5):
     return base * round(x/base)
 
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = AnalyzeVidWindow("", "VID_20201026_190145")
+    sys.exit(app.exec_())
